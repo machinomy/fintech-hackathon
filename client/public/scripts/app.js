61879fc717561d5f0691b0ddc9d18dd555dfc83a
Almir Davletov <davletovalmir@gmail.com>
2016-09-18 03:27:14 +0300

    magiya

--- client/public/scripts/app.js
+++ client/public/scripts/app.js
@@ -89,8 +89,13 @@ window.addEventListener('load', () => {
   });
 
   $('#passport_series').on('keyup', (e) => {
-    if ($(e.currentTarget).val().length === 4) $('#passport_number').trigger('focus');
-    if ($(e.currentTarget).val().length === 4) $('#passport_number').trigger('focus');
+    let v = $(e.currentTarget).val();
+    console.log(v);
+    if (v.length > 4) {
+      v = v.slice(0, 4);
+      $(e.currentTarget).val(v);
+    }
+    if (v.length === 4) $('#passport_number').trigger('focus');
   });
 
   $('.modal-trigger').leanModal();
